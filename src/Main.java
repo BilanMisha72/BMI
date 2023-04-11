afd174d740d636811d531da0fef7a5f09b2acf2b
Misha <bilan.md72@gmail.com>
2023-04-11 22:07:06 +0500

    calc

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int weightKg = 95;
+        double growthMetr = 1.88;
+        int BMI = service.calculate( weightKg, growthMetr );
+        System.out.println(BMI);
+
+    }
+}
